46132cde09b5c777e0186e2d7fb98b7e7f931991
Yassir Karroum <ukarroum17@gmail.com>
2016-04-07 19:39:32 +0100

    Ajout de l'option du déchiffrage

--- caesar.py
+++ caesar.py
@@ -53,7 +53,7 @@ def chiffrer(key, filein, fileout):
         if ord(inp[i]) + key <= ord('z'):
             inp[i] = chr(ord(inp[i]) + key)
         else:
-            inp[i] = chr(ord(c) + key - ord('z') + ord('a') - 1)
+            inp[i] = chr(ord(inp[i]) + key - ord('z') + ord('a') - 1)
 
     inp = "".join(inp) 
 
@@ -65,7 +65,37 @@ def chiffrer(key, filein, fileout):
     f.write(inp)
     f.close()
 
+def dechiffrer(filein, fileout):
+    """ Dechiffre un texte sans clé """
 
+    #Lecture
+    if filein == sys.stdin:
+        f = sys.stdin
+    else:
+        f = open(filein, 'r')
+    inp = f.read()
+    f.close()
+
+    # Calcul des frequences
+    freq = [0.0]*26
+
+    inp = inp.lower()
+    for i in range(len(inp)):
+        if inp[i] == ' ' or inp[i] == '\n':
+            continue
+        freq[ord(inp[i]) - ord('a')] += 1
+    for i in range(26):
+        freq[i] = freq[i] / len(inp)
+
+    # Calcul de la clé
+    charMax = chr(freq.index(max(freq)) + ord('a'))
+    
+    if charMax <= 'e':
+        chiffrer(ord('e') - ord(charMax), filein, fileout)
+    else:
+        chiffrer(26 - (ord(charMax) - ord('e')), filein, fileout)
+
+ 
 ###### Main ##############
 
 try:
@@ -91,9 +121,5 @@ for opt, arg in opts:
 
 if(g_opts['chiffrer']):
     chiffrer(g_opts['key'], g_opts['filein'], g_opts['fileout'])
-#else:
-    #dechiffrer(g_opts['filein'], g_opts['fileout'])
-
-
-         
-
+else:
+    dechiffrer(g_opts['filein'], g_opts['fileout'])
